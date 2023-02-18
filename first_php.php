db707ffdb51b5c29df57ca7fe3e98ef9908bbcf2
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-18 13:08:41 -0800

    php

--- first_php.php
+++ first_php.php
@@ -7,8 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        echo "Hello there!";
-    ?>
+    <form action="functions.php" method="get">
+        <input type="text" name="num01" placeholder="Number 1">
+        <select name="oper">
+            <label>Choose operation!</label>
+            <option value="add">Add</option>
+            <option value="sub">Subtract</option>
+        </select>
+        <input type="text" name="num02" placeholder="Number 2">
+        <button type="submit">Calculate</button>
+    </form>
 </body>
 </html>
